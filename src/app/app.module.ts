77d6e1d8eb32cbd99bd74710dcb8b1b4e69570e5
IgnaBiodre <ignacio.d.mendez@gmail.com>
2021-05-14 21:26:27 -0300

    master

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ReachComponent } from './reach/reach.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    ReachComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+   ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
