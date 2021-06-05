357fabbd1fc9fad45cad4a7ab468feadfbb92be0
IgnaBiodre <ignacio.d.mendez@gmail.com>
2021-06-04 21:04:52 -0300

    master

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+   
+  parrafo: string = '';
 
   constructor() { }
 
   ngOnInit(): void {
+    this.parrafo = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim explicabo ullam quasi deserunt iusto corrupti recusandae doloremque quo,magnam accusantium vero impedit optio, odio officia, rerum ipsa culpa fugiat quisquam."
   }
 
 }
--- src/app/reach/reach.component.ts
+++ src/app/reach/reach.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-reach',
   templateUrl: './reach.component.html',
   styleUrls: ['./reach.component.css']
 })
 export class ReachComponent implements OnInit {
+  
+  constructor() {}
+   ngOnInit(): void {
+  }
 
-  constructor() { }
+  submit(){
 
-  ngOnInit(): void {
   }
 
 }
