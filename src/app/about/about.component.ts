b48fd1f15854383b3d6ecf6790d873c358feecb2
IgnaBiodre <ignacio.d.mendez@gmail.com>
2021-07-20 14:54:23 -0300

    issues-2

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
 })
 export class AboutComponent implements OnInit {
+  parrafo: string =
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit Enim explicabo ullam quasi deserunt iusto corrupti recusandae doloremque quo, magnam accusantium vero impedit optio, odio officia, rerum ipsa culpa fugiat quisquam.';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
